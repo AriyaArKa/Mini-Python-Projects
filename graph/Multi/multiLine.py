074ecc5c60e406417d9fe789bc0069fb3ef445c5
AriyaArKa <nattharka55@gmail.com>
2024-07-04 12:33:20 +0600

    updated

--- graph/Multi/multiLine.py
+++ graph/Multi/multiLine.py
@@ -1,14 +1,14 @@
 import matplotlib.pyplot as plt  # Import the pyplot module
 
-subjects = ['bangla', 'english', 'physics', 'cse', 'eee', 'archi','hum']
-myMarks = [80,70,60,65,90,80,77]
-myFriendsMarks = [72,80,80,70,85,70,80]
+subjects = ['Buuble', 'Insertion', 'Quick', 'Selection', 'Merge', 'Heap','Radix','Counting','Bucket']
+# myMarks = [5181,1502,152,2795,673,314,215,159,998]
+myFriendsMarks = [5181,1502,152,2795,673,314,215,159,998]
 
-plt.plot(subjects,myMarks,label='my marks',marker='o',markerfacecolor='red',markersize=8,linestyle='dashdot')
+# plt.plot(subjects,myMarks,label='my marks',marker='o',markerfacecolor='red',markersize=8,linestyle='dashdot')
 plt.plot(subjects,myFriendsMarks,label='my friends marks',marker='o',markerfacecolor='yellow',markersize=8,linestyle='dashdot')
-plt.xlabel('subjects')
-plt.ylabel('marks')
-plt.title('marks comparison')
+plt.xlabel('Sortings')
+plt.ylabel('Time taken in microseconds')
+plt.title('Time taken when size is 1000')
 plt.legend()
 
 plt.show()
--- graph/line/lineGraph.py
+++ graph/line/lineGraph.py
@@ -1,13 +1,52 @@
 import matplotlib.pyplot as plt  # Import the pyplot module
 
-days = ['sun', 'mon', 'tues', 'thrus', 'fri', 'satur']
+days = ['Buuble', 'Insertion', 'Quick', 'Selection', 'Merge', 'Heap','Radix','Counting','Bucket']
 
 # The 'numbers' list has one extra element compared to 'days', 'satur' should be removed or another day should be added
-numbers = [25, 15, 20, 15, 18, 30]
+numbers = [626113,160694,1673,501094,5059,3912,2195,741,2992]
 
 plt.plot(days, numbers,color='orange',linewidth=5,marker='o',markerfacecolor='blue',markersize=12,linestyle='dashdot')
 
-plt.xlabel('Days')
-plt.ylabel('number of push ups')
-plt.title('my push up graph')
+plt.xlabel('Sortings')
+plt.ylabel('Time taken in microseconds')
+plt.title('Time taken when size is 10000')
 plt.show()
+
+# import matplotlib.pyplot as plt
+#
+# # Sample data
+# array_sizes = [1000, 5000, 10000]
+# bubble_sort_times = [5181, 149490, 626113]
+# insertion_sort_times = [1502, 45341, 160694]
+# Quick_sort_times = [152, 882, 1673]
+# Selection_sort_times = [2795, 161643, 501094]
+# merge_sort_times = [673, 3117, 5059]
+# Heap_sort_times = [314, 1863, 3912]
+# Radix_sort_times = [215, 1078, 2195]
+# Counting_sort_times = [159, 447, 741]
+# Bucket_sort_times = [998, 1995, 2992]
+#
+# # Plotting
+# plt.plot(array_sizes, bubble_sort_times, label='Bubble Sort')
+# plt.plot(array_sizes, insertion_sort_times, label='Insertion Sort')
+# plt.plot(array_sizes, Quick_sort_times, label='Quick Sort')
+# plt.plot(array_sizes, Selection_sort_times, label='Selection Sort')
+# plt.plot(array_sizes, merge_sort_times, label='Merge Sort')
+# plt.plot(array_sizes, Heap_sort_times, label='Heap Sort')
+# plt.plot(array_sizes, Radix_sort_times, label='Radix Sort')
+# plt.plot(array_sizes, Counting_sort_times, label='Counting Sort')
+# plt.plot(array_sizes, Bucket_sort_times, label='Bucket Sort')
+#
+#
+# # Labeling and customization
+# plt.xlabel('Array Size')
+# plt.ylabel('Time Taken (micros)')
+# plt.title('Sorting Algorithm Performance')
+# plt.legend()
+#
+# # Set logarithmic scale for y-axis
+# plt.yscale('log')
+#
+# # Show plot
+# plt.show()
+
